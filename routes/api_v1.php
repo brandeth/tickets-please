6a84c14954096cb4cc434e73b3c5d77979e51c73
Rey Garcia <jreygarcia24@gmail.com>
2024-11-29 21:03:26 +0800

    Added versioning for APIs.

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TicketController;
 use App\Http\Controllers\AuthController;
 use App\Models\Ticket;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
