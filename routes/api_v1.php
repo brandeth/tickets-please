27fe2efdc4f54820a9d81036a0ee1884b352fbba
Rey Garcia <jreygarcia24@gmail.com>
2024-11-30 17:33:05 +0800

    Added Auth Tokens.

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,10 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
