65a8e75c2790508b4296057f7c7673c1b0c8b7fd
Rey Garcia <jreygarcia24@gmail.com>
2024-12-05 17:43:45 +0800

    Added filters on user

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,10 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\Api\V1\UsersController;
+use App\Http\Controllers\Api\V1\AuthorsController;
+use App\Http\Controllers\Api\V1\AuthorTicketsController;
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('users', UsersController::class);
+Route::middleware('auth:sanctum')->apiResource('authors', AuthorsController::class);
+Route::middleware('auth:sanctum')->apiResource('authors.tickets', AuthorTicketsController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
